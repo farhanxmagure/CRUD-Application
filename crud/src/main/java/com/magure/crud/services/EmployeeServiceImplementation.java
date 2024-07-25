f58dcef2d8c5d1b087fc2468f2466d95fdb70087
farhanxmagure <farhan.k@magureinc.com>
2024-07-25 14:09:47 +0530

    Update EmployeeServiceImplementation.java

--- crud/src/main/java/com/magure/crud/services/EmployeeServiceImplementation.java
+++ crud/src/main/java/com/magure/crud/services/EmployeeServiceImplementation.java
@@ -5,11 +5,13 @@ import com.magure.crud.models.Employee;
 import com.magure.crud.repositories.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class EmployeeServiceImplementation implements EmployeeService {
 
     @Autowired
@@ -18,55 +20,56 @@ public class EmployeeServiceImplementation implements EmployeeService {
     @Override
     public String createEmployee(Employee employee) {
         EmployeeEntity employeeEntity = new EmployeeEntity();
-        employeeEntity.setEmail(employee.getEmail());
-        employeeEntity.setName(employee.getName());
-        employeeEntity.setPhoneNumber(employee.getPhoneNumber());
+        mapEmployeeToEntity(employee, employeeEntity);
         employeeRepository.save(employeeEntity);
         return "Employee created successfully";
     }
 
     @Override
     public List<Employee> readEmployees() {
-        List<EmployeeEntity> employeesList = employeeRepository.findAll();
-        List<Employee> employees = new ArrayList<>();
-
-        for (EmployeeEntity employeeEntity : employeesList) {
-            Employee emp = new Employee();
-            emp.setId(employeeEntity.getId());
-            emp.setEmail(employeeEntity.getEmail());
-            emp.setPhoneNumber(employeeEntity.getPhoneNumber());
-            emp.setName(employeeEntity.getName());
-
-            employees.add(emp);
-        }
-        return employees;
+        return employeeRepository.findAll().stream()
+                .map(this::mapEntityToEmployee)
+                .collect(Collectors.toList());
     }
 
     @Override
     public boolean deleteEmployee(Long id) {
-        EmployeeEntity existingEmployee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
-        employeeRepository.delete(existingEmployee);
-        return true;
+        if (employeeRepository.existsById(id)) {
+            employeeRepository.deleteById(id);
+            return true;
+        } else {
+            throw new RuntimeException("Employee not found");
+        }
     }
 
     @Override
     public String updateEmployee(Long id, Employee employee) {
-        EmployeeEntity existingEmployee = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
-        existingEmployee.setEmail(employee.getEmail());
-        existingEmployee.setName(employee.getName());
-        existingEmployee.setPhoneNumber(employee.getPhoneNumber());
+        EmployeeEntity existingEmployee = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
+        mapEmployeeToEntity(employee, existingEmployee);
         employeeRepository.save(existingEmployee);
-        return "Update successfully";
+        return "Employee updated successfully";
     }
 
     @Override
     public Employee readEmployee(Long id) {
-        EmployeeEntity employeeEntity = employeeRepository.findById(id).orElseThrow(() -> new RuntimeException("Employee not found"));
-        Employee employee = new Employee();
-        employee.setId(employeeEntity.getId());
-        employee.setEmail(employeeEntity.getEmail());
-        employee.setPhoneNumber(employeeEntity.getPhoneNumber());
-        employee.setName(employeeEntity.getName());
-        return employee;
+        EmployeeEntity employeeEntity = employeeRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Employee not found"));
+        return mapEntityToEmployee(employeeEntity);
+    }
+
+    private void mapEmployeeToEntity(Employee employee, EmployeeEntity employeeEntity) {
+        employeeEntity.setEmail(employee.getEmail());
+        employeeEntity.setName(employee.getName());
+        employeeEntity.setPhoneNumber(employee.getPhoneNumber());
+    }
+
+    private Employee mapEntityToEmployee(EmployeeEntity employeeEntity) {
+        return new Employee(
+                employeeEntity.getId(),
+                employeeEntity.getName(),
+                employeeEntity.getPhoneNumber(),
+                employeeEntity.getEmail()
+        );
     }
 }
